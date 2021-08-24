1effd930df8f78dc4b4b7ed57b12208dd3c5795f
Anna Gondring <32964281+zednon@users.noreply.github.com>
2021-08-24 15:37:08 -0400

    fdfd

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,38 @@
 // Assignment code here
-
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate");
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", () => {
+  let characters = letters;
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordTxt.value = generatePassword(length.value, characters);
+});
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+const generatePassword = (length, characters) => {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password Copied");
+});
